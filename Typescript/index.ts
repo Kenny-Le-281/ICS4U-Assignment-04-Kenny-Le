ea39c58e7f6e30ebce6fa4e06bc9ed49f68bb5d2
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2024-05-18 12:04:49 -0400

    ts

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,34 +1,48 @@
 /*
- * This is the PalindromeDepth program
+ * This is the Palindrome program
  *
  * @author  Kenny Le
  * @version 1.0
- * @since   2024-05-08
+ * @since   2024-05-17
  */
 
-/*
- * This function calculates the palindrome depth of a number
- */
-function PalindromeDepth(integer: number, currentDepth: number): number {
-    // reverse the integer
-    const reversedInteger: number = integer.toString().split('').reverse().join('')
-    // check if integer is a palindrome
-    if (integer == reversedInteger) {
-      return currentDepth
-    } else {
-      const sum: number = parseInt(integer) + parseInt(reversedInteger)
-      return PalindromeDepth(sum, currentDepth + 1)
-    }
+// This is the is Palindrome function
+function isPalindrome(inputNumber: number): boolean {
+  return inputNumber === reverseNumber(inputNumber)
+}
+
+// This is the reverseNumber function
+function reverseNumber(inputNumber: number): number {
+  let reversedNumber = 0
+  while (inputNumber > 0) {
+    // Extract the last digit
+    const digit = inputNumber % 10
+    // Append it to the reversed number
+    reversedNumber = reversedNumber * 10 + digit
+    // Remove the last digit from the original number
+    inputNumber = Math.floor(inputNumber / 10)
   }
-  
-  
-  const startInt: number = 10
-  const finishInt: number = 99
-  
-  for (let counter = startInt; counter <= finishInt; counter++) {
-    let intDepth: number = PalindromeDepth(counter, 0)
-    console.log(`${counter} is a depth ${intDepth} palindrome.`)
+  return reversedNumber
+}
+
+// This is the findDepth function
+function findDepth(inputNumber: number, currentDepth: number = 0): number {
+  if (isPalindrome(inputNumber)) {
+    return currentDepth
+  } else {
+    const reversedNumber = reverseNumber(inputNumber);
+    const sum = inputNumber + reversedNumber
+    return findDepth(sum, currentDepth + 1)
   }
-  
-  console.log("\nDone.")
-  
+}
+
+// Loops through all the two-digit numbers to see if they are a palindrome
+const startInt: number = 10
+const finishInt: number = 99
+
+for (let counter = startInt; counter <= finishInt; counter++) {
+  let intDepth: number = findDepth(counter, 0)
+  console.log(`Number: ${counter}, Depth: ${intDepth}`)
+}
+
+console.log("\nDone.")
